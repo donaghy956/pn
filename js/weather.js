4cd3d6f8031451b16f17de25bff021e9e40ee430
donaghy956 <952957407@qq.com>
2021-11-05 19:52:13 +0800

    Site updated: 2021-11-05 19:52:11

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "180",
+    "top": "20",
+    "key": "4448a8808ef5478697d78a84846654c6"
+  }
+}
